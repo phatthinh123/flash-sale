3a543caaa9b10ccd1781fe60f9533fc645e91982
Thinh <phatthinh123@gmail.com>
2026-05-11 17:09:09 +0000

    Initialize the project

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.4.5" apply false
+    id("io.spring.dependency-management") version "1.1.7" apply false
+}
+
+allprojects {
+    group = "com.flashsale"
+    version = "0.0.1-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "io.spring.dependency-management")
+
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
+
+    dependencies {
+        implementation(platform("org.springframework.boot:spring-boot-dependencies:3.4.5"))
+        implementation(platform("org.springframework.cloud:spring-cloud-dependencies:2024.0.0"))
+    }
+
+    dependencies {
+        compileOnly("org.projectlombok:lombok")
+        annotationProcessor("org.projectlombok:lombok")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+}
